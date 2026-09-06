3e3f2061870f335be9785357d483425922cb852f
ttlt on github ig <agamedev77@gmail.com>
2026-09-06 10:54:43 +0300

    pwsh thing

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,20 @@
+try {
+    [Console]::InputEncoding  = [System.Text.Encoding]::UTF8
+    [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+    $OutputEncoding = [System.Text.UTF8Encoding]::new($false)
+    chcp 65001 > $null
+} catch {}
+
+Clear-Host
+
+# Force Fastfetch to use YOUR config every time (bypass path confusion)
+if (Get-Command fastfetch -ErrorAction SilentlyContinue) {
+    fastfetch -c "C:/Users/calin/.config/fastfetch/config.jsonc"
+}
+
+oh-my-posh init pwsh --config "$HOME\.config\oh-my-posh\config.omp.json" | Invoke-Expression
+
+#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
+
+Import-Module -Name Microsoft.WinGet.CommandNotFound
+#f45873b3-b655-43a6-b217-97c00aa0db58
